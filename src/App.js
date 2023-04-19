6a87ef1238812f256eca79b33345154d34952e7c
delanebrandy <delanebrandy13@outlook.com>
2023-04-19 14:26:16 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Spot from './Spot';
+import Main from './pages/main';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-        <Spot />
+      <Routes>
+        <Route path="/" element={<main />} />
+        <Route path="/app" element={<Main />} />
+      </Routes>
+      <Spot />
     </div>
   );
 }
